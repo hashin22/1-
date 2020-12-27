d115bf664f458f0d5cfabc1bda08492df2784dcd
haran <hashin22@naver.com>
2020-12-27 21:09:46 +0900

    k

---
+++ TestName03/src/MenuMain.java
@@ -0,0 +1,23 @@
+import java.awt.EventQueue;
+
+import javax.swing.JPanel;
+
+public class MenuMain {
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FirstScreen frame = new FirstScreen();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
